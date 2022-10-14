9955fcdc0b96504daa8638cde42d0eb12e00b3bf
Danish Shah <shah.danish.1996@gmail.com>
2022-10-14 15:20:55 +0200

    fix(layout): fix container layout

--- src/components/containers/AppContainer.tsx
+++ src/components/containers/AppContainer.tsx
@@ -1,47 +1,31 @@
-import { Route, Routes } from 'react-router'
+import { Outlet, Route, Routes, useNavigate } from 'react-router'
 
-import Box from 'components/Box'
 import Landing from 'pages/Landing'
 import Login from 'pages/Login'
 import SignUp from 'pages/Signup'
-import ProtectedRoute, { SessionState } from 'components/routes/ProtectedRoute'
 import ExternalLayout from 'layouts/ExternalLayout'
+import { useCurrentAccountContext } from 'contexts/CurrentAccountContext'
 
-const AppContainer: React.FC = () => (
-  <Box as="main" css={{ display: 'flex', height: '100vh', overflow: 'hidden' }}>
-    <Routes>
-      <Route
-        path="/"
-        element={(
-          <ProtectedRoute
-            component={Landing}
-            layout={ExternalLayout}
-            requiredSessionState={SessionState.LOGGED_OUT}
-          />
-      )}
-      />
-      <Route
-        path="/login"
-        element={(
-          <ProtectedRoute
-            component={Login}
-            layout={ExternalLayout}
-            requiredSessionState={SessionState.LOGGED_OUT}
-          />
-      )}
-      />
-      <Route
-        path="/signup"
-        element={(
-          <ProtectedRoute
-            component={SignUp}
-            layout={ExternalLayout}
-            requiredSessionState={SessionState.LOGGED_OUT}
-          />
-      )}
-      />
-    </Routes>
-  </Box>
-)
+const AppContainer: React.FC = () => {
+  const navigate = useNavigate()
+  const currentAccount = useCurrentAccountContext()
+  if (currentAccount) {
+    navigate('/')
+  }
+  if (!currentAccount) {
+    navigate('/login')
+  }
+
+  return (
+    <ExternalLayout>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <Outlet />
+    </ExternalLayout>
+  )
+}
 
 export default AppContainer
--- src/components/containers/WorkspaceContainer.tsx
+++ src/components/containers/WorkspaceContainer.tsx
@@ -1,71 +1,21 @@
-import { Route, Routes } from 'react-router'
+import { Outlet, Route, Routes } from 'react-router'
 
 import Schema from 'pages/Database/Schema'
 import Overview from 'pages/Overview'
 import RootPage from 'pages/RootPage'
-import Login from 'pages/Login'
-import SignUp from 'pages/Signup'
-import ProtectedRoute, { SessionState } from 'components/routes/ProtectedRoute'
 import InternalLayout from 'layouts/InternalLayout'
-import ExternalLayout from 'layouts/ExternalLayout'
 
 const WorkspaceContainer: React.FC = () => (
-  <Routes>
-    <Route
-      path="/"
-      element={(
-        <ProtectedRoute
-          component={RootPage}
-          layout={InternalLayout}
-          requiredSessionState={SessionState.LOGGED_IN}
-        />
-      )}
-    />
-
-    <Route
-      path="/login"
-      element={(
-        <ProtectedRoute
-          component={Login}
-          layout={ExternalLayout}
-          requiredSessionState={SessionState.LOGGED_OUT}
-        />
-      )}
-    />
-    <Route
-      path="/signup"
-      element={(
-        <ProtectedRoute
-          component={SignUp}
-          layout={ExternalLayout}
-          requiredSessionState={SessionState.LOGGED_OUT}
-        />
-      )}
-    />
-
-    <Route
-      path="/overview"
-      element={(
-        <ProtectedRoute
-          component={Overview}
-          layout={InternalLayout}
-          requiredSessionState={SessionState.LOGGED_IN}
-        />
-      )}
-    />
-    <Route path="/database">
-      <Route
-        path="/database/schema"
-        element={(
-          <ProtectedRoute
-            component={Schema}
-            layout={InternalLayout}
-            requiredSessionState={SessionState.LOGGED_IN}
-          />
-      )}
-      />
-    </Route>
-  </Routes>
+  <InternalLayout>
+    <Routes>
+      <Route path="/" element={<RootPage />} />
+      <Route path="/overview" element={<Overview />} />
+      <Route path="/database">
+        <Route path="/database/schema" element={<Schema />} />
+      </Route>
+    </Routes>
+    <Outlet />
+  </InternalLayout>
 )
 
 export default WorkspaceContainer
